56b53ca9add7a941dee924a30cccc2a6a56b732b
Aporbo <aporbogosh249@gmail.com>
2022-05-09 01:31:43 +0600

    added login system with some error

--- src/CSE115 project.c
+++ src/CSE115 project.c
@@ -8,6 +8,13 @@ struct book
     char name[50], stock[20], code[50];
     int price;
 };
+struct Login
+{
+    char fname[25];
+    char lname[15];
+    char username[10];
+    int password,id;
+};
 void add(struct book a[], int size);
 void view(struct book b[], int n);
 void calculate(struct book c[], int m);
@@ -118,32 +125,25 @@ int main()
     //for customer
     if(userType==2)
     {
-    printf("\n\n\t\t\t\tEnter username : ");
-    fflush(stdin);
-    gets(user);
-    printf("\t\t\t\tEnter password : ");
-    fflush(stdin);
-    gets(pass);
-        if (strcmp(user, "customer") == 0)
-        {
-            if (strcmp(pass, "123") == 0)
-            {
-                printf("\t\t\t\tLogin successful!\n\n");
-            }
-            else
-            {
-                printf("Invalid username or password! Please try again.");
-                return 0;
-            }
-        }
-        else
-        {
-            printf("Invalid username!");
-            return 0;
-        }
-
-        system("cls");
+    int cho;
+    printf("Press '1' for Register........... \nPress '2' for Login.........\n");
+    scanf("%d",&cho);
+    if(cho == 1)
+    {
+        system("CLS");
+        registe();
+    }
+    else if(cho == 2)
+    {
+        system("CLS");
+        login();
+    }
+    else
+        printf("Incorrect option !!!!!!!!!!!.......Please correct option secleted");
+            system("cls");
         system("color E1");
+        if(login()){
+
         while (1)
         {
         printf("\n\t\t\t\t\t%s",ctime(&now));
@@ -179,6 +179,8 @@ int main()
                 printf("\t\t\t\tInvalid choice!");
             }
         }
+        }
+
     }
     else
     {
@@ -189,7 +191,74 @@ int main()
 
 
 //all the functions are here
+registe()
+{
+    FILE *log;
+    log  = fopen("dataStoreFile.txt","w");
+    if(log == NULL)
+        printf("File doest not opened,some thing is error\n");
+    else
+    {
+        printf("File create successfully\n");
+        struct Login person1;
 
+        printf("\nEnter Person First name : ");
+        fflush(stdin);
+        gets(person1.fname);
+
+        printf("Enter Person Last name : ");
+        gets(person1.lname);
+
+        printf("Enter Person user name : ");
+        gets(person1.username);
+
+        printf("Enter Person ID : ");
+        scanf("%d",&person1.id);
+
+        printf("Enter Person Password : ");
+        scanf("%d",&person1.password);
+        fwrite(&person1,sizeof(person1),1,log);
+        printf("\nFile data written successful");
+
+        fclose(log);
+
+        printf("\n\nYour user name is your UserID\n");
+        printf("Now,Login with UserID and Password\n");
+        printf("\nPress any key to continue......................\n");
+        getch();
+        system("CLS");
+        login();
+    }
+}
+login()
+{
+    char username[10];
+    int password;
+    FILE *log;
+    log  = fopen("dataStoreFile.txt","r");
+    if(log == NULL)
+        printf("File doest not opened,some thing is error\n");
+    else
+    {
+        struct Login person1;
+        printf("UserID : ");
+        scanf("%s",&username);
+        printf("Password : ");
+        scanf("%d",&password);
+        while(fread(&person1,sizeof(person1),1,log))
+        {
+            if(strcmp(username,person1.username) == 0 && person1.password == password)
+            {
+                printf("Successfully Login\n");
+            }
+            else
+            {
+                printf("Please !!!!!.......... Enter your correct UserID and Password ");
+            }
+        }
+        fclose(log);
+    }
+}
 void add(struct book a[], int size)
 {
     int i, option;
